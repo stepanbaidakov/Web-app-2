d84f3689176d03fd3a9b4b8bca28d367eb713b9b
Stepan Baidakov <stepanbaidakov@icloud.com>
2026-02-15 15:22:13 +0100

    homework improved

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home", views.home, name="home"),
-    path("contacts", views.contacts, name="contacts"),
+    path("home/", views.home, name="home"),
+    path("contacts/", views.contacts, name="contacts"),
 ]
