eda2c01c346a8638c1aaa24377f64ace16e16d58
Stepan Baidakov <stepanbaidakov@icloud.com>
2026-03-03 15:45:58 +0100

    homework done

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
+    path("product_info/<int:product_id>/", views.product_info, name="product_info"),
+    path("add_product/", views.add_product, name="add_product"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from catalog.models import Product, ContactInfo
+from catalog.models import Product, ContactInfo, Category
 
 
 # Create your views here.
@@ -10,11 +10,36 @@ def home(request):
     print("Последние 5 продуктов:")
     for product in latest_products:
         print(product)
-    return render(request, "home.html", )
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "home.html", context)
 
 def contacts(request):
     contact_infos = ContactInfo.objects.all()
+    context = {"contact_infos": contact_infos}
     if request.method == "POST":
         name = request.POST.get("name")
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, "contacts.html", {"contact_infos": contact_infos})
+    return render(request, "contacts.html", context)
+
+def product_info(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {"product": product}
+    return render(request, "product_info.html", context)
+
+def add_product(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        category_name = request.POST.get("category")
+        category_obj, _ = Category.objects.get_or_create(name=category_name)
+        Product.objects.create(
+            name=name,
+            price=price,
+            description=description,
+            category=category_obj
+        )
+
+        return redirect("add_product")
+    return render(request, "add_product.html",)
